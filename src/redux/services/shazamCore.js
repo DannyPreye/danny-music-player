dbe4c1e526abed493a61802e5fdbdb4f4846c1f0
Olawoyin Daniel <olawoyindaniel95@gmail.com>
2023-06-05 16:39:15 +0100

    Update shazamCore.js

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -6,20 +6,20 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const shazamCoreApi = createApi({
     reducerPath: 'shazamCoreApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+        baseUrl: 'https://shazam-core7.p.rapidapi.com',
         prepareHeaders: (headers) => {
             headers.set('X-RapidAPI-Key', 'f5fc1abd73msha019e2a810140f0p117d2fjsn21c1f292ca40');
             return headers;
         }
     }),
     endpoints: (builder) => ({
-        getTopCharts: builder.query({ query: () => '/charts/world' }),
-        getSongsByGenre: builder.query({ query: (genre) => `/charts/genre-world?genre_code=${genre}` }),
-        getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
-        getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
-        getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
-        getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
-        getSongsBySearch: builder.query({ query: (searchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
+        getTopCharts: builder.query({ query: () => '/charts/get-top-songs-in-world' }),
+        getSongsByGenre: builder.query({ query: (genre) => `/charts/get-top-songs-in_world_by_genre?genre=${genre}` }),
+        getSongDetails: builder.query({ query: ({ id }) => `/songs/get_details?id=${id}` }),
+        getSongRelated: builder.query({ query: ({ songid }) => `/songs/list-recommendations?id=${songid}` }),
+        getArtistDetails: builder.query({ query: (artistId) => `/artist/get-details?id=${artistId}` }),
+        getSongsByCountry: builder.query({ query: (countryCode) => `/charts/get-top-songs-in-country?country_code=${countryCode}` }),
+        getSongsBySearch: builder.query({ query: (searchTerm) => `/search?term=${searchTerm}` }),
     }),
 
 });
